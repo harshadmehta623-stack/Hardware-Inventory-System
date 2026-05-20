3afa355d6fb14e35b69c0695987009ab86631e28
harshadmehta623-stack <harshadmehta623@gmail.com>
2026-05-20 23:26:43 +0800

    Add files via upload

---
+++ hardware-inventory-system/admin/HWAdmin/AdminSettings.cs
@@ -0,0 +1,9 @@
+namespace ComputerHardwareStockMonitoringSystem
+{
+    public static class AdminSettings
+    {
+        public const string AdminUsername = "admin";
+        public const string AdminPassword = "admin123";
+        public const string AdminApiToken = "change-this-local-admin-token";
+    }
+}
